06e8e20b7939602427e9dfef6f4fb515d70ccc34
Kallln <ahmad.haikal41@ui.ac.id>
2026-05-03 22:17:04 +0700

    Transaction Service Implementation

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -1,4 +1,7 @@
-use services::{payment_service_client::PaymentServiceClient, PaymentRequest};
+use services::{
+    payment_service_client::PaymentServiceClient, PaymentRequest,
+    transaction_service_client::TransactionServiceClient, TransactionRequest
+};
 
 pub mod services {
     tonic::include_proto!("services");
@@ -6,18 +9,28 @@ pub mod services {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Connect to the server
+    // 1. Unary RPC: Payment Service
     let mut client = PaymentServiceClient::connect("http://[::1]:50051").await?;
-
-    // 1. Create and send the request
     let request = tonic::Request::new(PaymentRequest {
         user_id: "user_123".to_string(),
         amount: 100.0,
     });
 
     let response = client.process_payment(request).await?;
-    
     println!("RESPONSE={:?}", response.into_inner());
+
+    // 2. Server-Side Streaming RPC: Transaction Service
+    let mut transaction_client = TransactionServiceClient::connect("http://[::1]:50051").await?;
+    let request = tonic::Request::new(TransactionRequest {
+        user_id: "user_123".to_string(),
+    });
+
+    let mut stream = transaction_client.get_transaction_history(request).await?.into_inner();
+    
+    // Process the stream of transactions
+    while let Some(transaction) = stream.message().await? {
+        println!("Transaction: {:?}", transaction);
+    }
 
     Ok(())
 }
